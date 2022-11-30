dcf15eff5d8f4e9b8a8532315edf68af79768499
LudrahsGartage <101329074+LudrahsGartage@users.noreply.github.com>
2022-11-30 22:14:42 +0530

    button conditional rendering

--- Components/Option.jsx
+++ Components/Option.jsx
@@ -1,8 +1,26 @@
 import React from "react"
 
 export default function Option (props) {
-    console.log(props)
+    
+    function determineClass () {
+        let style = "options "
+        if (props.screen === "Playing"){
+            if (props.playerResponse.answer === props.answer) {
+                style = "selected"
+            } 
+        } else if (props.screen === "Submitted") {
+            if (props.playerResponse.answer === props.answer && props.playerResponse.answer === props.correctAnswer) {
+                style = "correct-answer"
+            } else if (props.playerResponse.answer === props.answer && props.playerResponse.answer !== props.correctAnswer) {
+                style = "incorrect-answer"
+            }
+        }
+        return style
+    }
+
+    const classlist = `options ${determineClass()}`
+
     return (
-        <button className="options">{props.answer}</button>
+        <button className={classlist} >{props.answer}</button>
     )
 }
--- Components/Question.jsx
+++ Components/Question.jsx
@@ -2,8 +2,7 @@ import React from "react"
 import Option from "./Option"
 
 export default function Question (props) {
-    const options = props.data.optionList.map((option,i) => <Option answer={option} key={i} playerResponse={props.playerResponse} screen={props.screen}/>)
-    
+    const options = props.data.optionList.map((option,i) => <Option answer={option} correctAnswer={props.data.correctAnswer} key={i} playerResponse={props.playerResponse} screen={props.screen}/>)
     return (
         <div className="question-container">
             <h2 className="questions">{props.data.question}</h2>
