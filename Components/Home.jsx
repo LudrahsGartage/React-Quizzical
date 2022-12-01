e9db2775a45e263bf7ada128faaa53b9326e2dcb
LudrahsGartage <101329074+LudrahsGartage@users.noreply.github.com>
2022-12-01 22:58:28 +0530

    Change in title screen

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home (props) {
     return (
         <>
             <h1 className="title">Quizzical</h1>
-            <p className="description">Test your knowledge!</p>
+            <p className="description">Time for Television Trivia!</p>
             <button onClick={props.handleClick} className="start-game">Start Quiz</button>
         </>
     )
