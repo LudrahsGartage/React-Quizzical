797d150ef2c6c845b4ff3e7316197ad5c90e26da
LudrahsGartage <101329074+LudrahsGartage@users.noreply.github.com>
2022-12-01 22:41:17 +0530

    fixed correct answer bug

--- Components/Option.jsx
+++ Components/Option.jsx
@@ -9,7 +9,7 @@ export default function Option (props) {
                 style = "selected"
             } 
         } else if (props.screen == "End") {
-            if (props.playerResponse.playerAnswer== props.answer && props.playerResponse.playerAnswer == props.correctAnswer) {
+            if (props.answer == props.correctAnswer) {
                 style = "correct-answer"
             } else if (props.playerResponse.playerAnswer == props.answer && props.playerResponse.playerAnswer !== props.correctAnswer) {
                 style = "incorrect-answer"
